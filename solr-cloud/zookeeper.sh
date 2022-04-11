9d209a5a39ff125554c995d2f375b74c465e0f85
Paul Mitchell <39264461+Lithium007@users.noreply.github.com>
2022-04-11 17:12:49 +0100

    Update zookeeper.sh
    Restored to original

--- solr-cloud/zookeeper.sh
+++ solr-cloud/zookeeper.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#wget --no-check-certificate --no-cookies "https://files-cdn.liferay.com/mirrors/download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz"
-#wget --no-check-certificate --no-cookies "https://files-cdn.liferay.com/mirrors/download.oracle.com/otn-pub/java/jdk/11.0.13+10/bdde8881e2e3437baa70044f884d2d67/jdk-11.0.13_linux-x64_bin.tar.gz"
-wget --no-check-certificate --no-cookies "https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz"
-tar -xvf jdk-1*
+wget --no-check-certificate --no-cookies "https://files-cdn.liferay.com/mirrors/download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz"
+tar -xvf jdk-7*
 mkdir /usr/lib/jvm
-mv ./jdk1* /usr/lib/jvm/jdk18
-update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk18/bin/java" 1
-update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk18/bin/javac" 1
-update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk18/bin/javaws" 1
+mv ./jdk1.7* /usr/lib/jvm/jdk1.7.0
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
 chmod a+x /usr/bin/java
 chmod a+x /usr/bin/javac
 chmod a+x /usr/bin/javaws
@@ -32,20 +30,8 @@ do
     i=$(($i+1))
 done
 
-echo "autopurge.snapRetainCount=3" >> zookeeper-3.4.12/conf/zoo.cfg
-echo "autopurge.purgeInterval=1" >> zookeeper-3.4.12/conf/zoo.cfg
-
 mkdir -p /var/lib/zookeeper
 
 echo $(($1+1)) >> /var/lib/zookeeper/myid
 
 zookeeper-3.4.12/bin/zkServer.sh start
-
-#JAVA_HOME update
-echo JAVA_HOME="/usr/lib/jvm/jdk18" >> /etc/environment
-
-#Solr
-wget --no-check-certificate --no-cookies "https://www.apache.org/dyn/closer.lua/lucene/solr/8.11.1/solr-8.11.1.tgz?action=download" -O ./solr-8.11.1.tgz
-tar xvf ./solr-8.11.1.tgz
-# ./solr-8.11.1.tgz/bin/solr zk mkroot /solr -z zk1:2181,zk2:2181,zk3:2181
-# ./solr-8.11.1.tgz/bin/solr start -e cloud -z zk1:2181,zk2:2181,zk3:2181/solr
